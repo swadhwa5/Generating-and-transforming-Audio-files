abbd9503c6b2c50a7ba2655fdc4239b405d102ed
Shreya Wadhwa <swadhwa5@ugradx.cs.jhu.edu>
2019-10-15 16:06:17 -0400

    render_song working on first test song

--- render_song.c
+++ render_song.c
@@ -9,14 +9,15 @@
 
 int main(int argc, char** argv) {
   if(argc != 3) {
-  fatal_error("Malformed Input");
+  fatal_error("Enter appropriate command line arguments");
   exit(1);
   }
 
   unsigned long int num_samples;
   unsigned long int num_samples_per_beat;
   FILE *in = fopen(argv[1],"r");
-  scanf("%lu%lu",&num_samples,&num_samples_per_beat);
+  FILE *out = fopen(argv[2],"wb");
+  fscanf(in,"%lu%lu",&num_samples,&num_samples_per_beat);
 
 
   int voice = 0;
@@ -28,64 +29,77 @@ int main(int argc, char** argv) {
   float note_number;
   float new_num_samples;
   float freq_hz;
-  int16_t address;
-  float currvalue;
+  char *message;
+  unsigned long int var = 0;
+  
   
   
   while (!feof(in)) {
-    scanf(" %c",&type);
+    fscanf(in," %c",&type);
     switch(type) {
 
     case 'N':
       
-      scanf("%f",&num_beats);
+      fscanf(in,"%f",&num_beats);
       
-      scanf("%f",&note_number);
+      fscanf(in,"%f",&note_number);
 
       freq_hz = 440*pow(2,((note_number-69) / 12));
       new_num_samples = num_beats * num_samples_per_beat;
-      render_voice_stereo(data, new_num_samples, freq_hz, amplitude, voice);
+ 
+      render_voice_stereo(&data[var], new_num_samples, freq_hz, amplitude, voice);
+
+
+      var += new_num_samples*2;
+      
       break;
 
     case 'C':
 
-      address = &data;
-      scanf("%f",&num_beats);
       
-      scanf("%f",&note_number);
+      fscanf(in,"%f",&num_beats);
+      
+      fscanf(in,"%f",&note_number);
       while(note_number != 999) {
-	&data = address;
+	
 
 	freq_hz = 440*pow(2,((note_number-69) / 12));
 	new_num_samples = num_beats * num_samples_per_beat;
-	render_voice_stereo(data, new_num_samples, freq_hz, amplitude, voice);
-	scanf("%f",&note_number);
+
+	render_voice_stereo(&data[var], new_num_samples, freq_hz, amplitude, voice);
+
+
+	fscanf(in,"%f",&note_number);
       }
+      var +=new_num_samples*2;
 	break;
 
     case 'P':
-      scanf("%f",&num_beats);
-      currvalue = *data;
-      for (int i = 0; i< num_beats;i++) {
-	
-	*data = currvalue;
-	data++;
-      }
+      fscanf(in,"%f",&num_beats);
+      new_num_samples = num_beats * num_samples_per_beat;
+      var +=new_num_samples*2;
       break;
       
-    case 'V':scanf("%d",&voice);
+    case 'V':fscanf(in,"%d",&voice);
       break;
 
-    case 'A':scanf("%f",&amplitude);
+    case 'A':fscanf(in,"%f",&amplitude);
       break;
 
-    default:printf("Wrong Directive");
+    default:message = "Wrong Directive";//check for end of file
+      fatal_error(message);
+      
       break;
     }
    
     
   }
+
+  write_wave_header(out, num_samples);
+  write_s16_buf(out, data, num_samples*2u);
+    
   fclose(in);
+  fclose(out);
   
 }
   
